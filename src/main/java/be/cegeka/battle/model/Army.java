e3e7729a3e5933fd1a2ed582ab1a88cee84af7a3
florinciu <florinciu@gmail.com>
2018-03-30 15:07:40 +0300

    checking if army is empty

--- src/main/java/be/cegeka/battle/model/Army.java
+++ src/main/java/be/cegeka/battle/model/Army.java
@@ -22,6 +22,10 @@ public class Army {
     }
 
     public void killFrontSoldier() {
-        soldiers.remove(this.getFrontSoldier());
+        Soldier frontSoldier = getFrontSoldier();
+        if(frontSoldier == null) {
+            throw new RuntimeException("Army has no more soldiers.");
+        }
+        soldiers.remove(frontSoldier);
     }
 }
